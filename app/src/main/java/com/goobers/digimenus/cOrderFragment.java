6054843f6edcd8ba0c5f8a17e741b4d5c88ba4d0
tylerdenney <tjay930@hotmail.com>
2015-01-19 15:52:36 -0800

    Network code works on this end

--- app/src/main/java/com/goobers/digimenus/cOrderFragment.java
+++ app/src/main/java/com/goobers/digimenus/cOrderFragment.java
@@ -45,6 +45,7 @@ public class cOrderFragment extends Fragment implements View.OnClickListener
     private TextView txtparty;
     private TextView txttable;
     private TextView txtcost;
+    private static TextView txtrequeststatus;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -74,6 +75,7 @@ public class cOrderFragment extends Fragment implements View.OnClickListener
         super.onActivityCreated(savedInstanceState);
         myitems = (ListView) getView().findViewById(R.id.listView);
         List<iItem> items = cMenu.GetOrderedItems();
+
         _items = new ArrayList<iItem>();
         _items = items;
         if(items != null)
@@ -90,6 +92,7 @@ public class cOrderFragment extends Fragment implements View.OnClickListener
         txtparty =(TextView)getActivity().findViewById(R.id.txtpartysize);
         txttable =(TextView)getActivity().findViewById(R.id.txttablenum);
         txtcost =(TextView)getActivity().findViewById(R.id.txttotalcost);
+        txtrequeststatus =(TextView)getActivity().findViewById(R.id.txtrequest);
 
         txtcost.setText(getString(R.string.total_cost) + String.valueOf(cMenu.GetCost()));
         txttable.setText(getString(R.string.table_num) + String.valueOf(cMenu.GetTableNum()));
@@ -133,11 +136,10 @@ public class cOrderFragment extends Fragment implements View.OnClickListener
         return true;
     }
 
-    private static class SendRealRequest extends AsyncTask<String, byte[], Boolean>
+    //Subclass for AsyncTask that will run on a separate thread to not lock up system.
+    private static class SendRealRequest extends AsyncTask<String, Integer, Boolean>
     {
         Socket socket;
-        InputStream is;
-        OutputStream os;
         BufferedReader bio;
         BufferedWriter bwo;
         protected Boolean doInBackground(String... args)
@@ -147,33 +149,26 @@ public class cOrderFragment extends Fragment implements View.OnClickListener
             {
                 SocketAddress address = new InetSocketAddress("192.168.1.2", 4444);
                 socket = new Socket();
-                socket.connect(address, 5000);
+                socket.connect(address, 5000);//5000ms timeout
                 if(socket.isConnected())
                 {
-
                     bio = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     bwo = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    publishProgress(2);
+                    //TODO: format data to be sent
                     bwo.write("Hello");
                     bwo.flush();
 
-                   // is = socket.getInputStream();
-                   //os = socket.getOutputStream();
-                   //byte[] buffer = new byte[256];
-
                     String incoming = "";
-                    boolean b = false;
-                    //int read = is.read(buffer, 0, 1);
-                    while(!b)
+                    boolean done = false;
+
+                    while(!done)
                     {
                         incoming = bio.readLine();
-                        if(incoming != "")
-                            b = true;
-                        //byte[] tempdata = new byte[read];
-                       // System.arraycopy(buffer, 0, tempd ata, 0, read);
-                       // publishProgress(tempdata);
-                        //Log.i("AsyncTask", "doInBackground: Got some data");
-                        //read = is.read(buffer, 0, 4096);
+                        if(incoming != "")//if it reads a newline/nothing
+                            done = true;
                     }
+                    publishProgress(1);//When connection is done, update ui and close connection
                 }
             }
             catch(IOException e)
@@ -193,8 +188,8 @@ public class cOrderFragment extends Fragment implements View.OnClickListener
             {
                 try
                 {
-                    is.close();
-                    os.close();
+                    bio.close();
+                    bwo.close();
                     socket.close();
                 }
                 catch(IOException e)
@@ -209,12 +204,17 @@ public class cOrderFragment extends Fragment implements View.OnClickListener
             return result;
         }
 
-        protected void onProgressUpdate(byte[]... values)
+        protected void onProgressUpdate(Integer... values)
         {
-            if(values.length > 0)
+            if(values[0] == 1)
             {
-
+                txtrequeststatus.setText("Message Received");
             }
+            else if(values[0] == 2)
+            {
+                txtrequeststatus.setText("Awaiting Confirmation");
+            }
+
         }
     }
 
